611d0d553c9c08c9d22f06b3c8caa46b39018a91
Gala Anania <gsanania@gmail.com>
2020-08-11 16:22:16 +0200

    added the index user story

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,10 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    raise
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
